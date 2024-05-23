b47ead004595e7b31c05ecfe636965321163c484
Hieutritue <124784640+Hieutritue@users.noreply.github.com>
2024-05-24 05:30:15 +0700

    add edit profile form validation

--- src/page/user-profile/UserProfilePage.tsx
+++ src/page/user-profile/UserProfilePage.tsx
@@ -3,33 +3,20 @@ import { useSearchParams } from "react-router-dom";
 import axios from "axios";
 import { ProfileCard } from "../../component/user-profile/ProfileCard";
 import { Box } from "@mantine/core";
+import { emptyUser, fetchUser } from "../../util/UserFetchUtil";
 
-export const emptyUser = {
-  username: "",
-  firstName: "",
-  lastName: "",
-  email: "",
-  role: "",
-  status: "",
-};
 export default function UserProfilePage() {
-  
-  const [queryParameters] = useSearchParams();
-  const username = queryParameters.get("username");
-  const api = "http://localhost:8080/api/v1/user?username="+username;
   const [user, setUser] = useState(emptyUser);
   useEffect(() => {
-    const fetch = async () => {
-      console.log(api);
-      await axios.get(api).then((res) => {
-        setUser(res.data);
-      });
+    const fetchData = async () => {
+      const res = await fetchUser();
+      setUser(res.data);
     };
-    fetch();
-  } ,[]);
-  console.log("duoiu");
-  return <>
-  <ProfileCard user={user}/>
-  
-  </>;
+    fetchData();
+  }, []);
+  return (
+    <>
+      <ProfileCard user={user} />
+    </>
+  );
 }
