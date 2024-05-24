efd2310600ede327088ac8770c2b195af414dc71
Hai Vu <vmhtl2004@gmail.com>
2024-05-24 18:28:00 +0700

    add: landing page & components

--- src/page/landing-page/LandingPage.tsx
+++ src/page/landing-page/LandingPage.tsx
@@ -1,3 +1,19 @@
+import { Title } from "@mantine/core";
+import Carousel from "../../../components/carousel/HomeCarousel";
+import HeroContent from "../../../components/hero/HeroContent";
+import DocumentTitle from "../../../components/document-title/DocumentTitle";
+
 export default function LandingPage() {
-    return <>LandingPage</>;
+    DocumentTitle("FU-4S");
+    return (
+        <div className="overflow-x-hidden">
+            <HeroContent />
+            <div className="p-10 mx-auto">
+                <Title className="text-center text-4xl">
+                    Why you should use FU-4S
+                </Title>
+            </div>
+            <Carousel />
+        </div>
+    );
   }
