67b2cd2777547ad56cc55fcde02a64189d6ce1ca
Hai Vu <vmhtl2004@gmail.com>
2024-05-24 18:31:52 +0700

    add files & update paths in LandingPage

--- src/page/landing-page/LandingPage.tsx
+++ src/page/landing-page/LandingPage.tsx
@@ -1,7 +1,7 @@
 import { Title } from "@mantine/core";
-import Carousel from "../../../components/carousel/HomeCarousel";
-import HeroContent from "../../../components/hero/HeroContent";
-import DocumentTitle from "../../../components/document-title/DocumentTitle";
+import Carousel from "../../component/carousel/HomeCarousel";
+import HeroContent from "../../component/hero/HeroContent";
+import DocumentTitle from "../../component/document-title/DocumentTitle";
 
 export default function LandingPage() {
     DocumentTitle("FU-4S");
