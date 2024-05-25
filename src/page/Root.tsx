9cbba4eee15ac8aa1de8f9b4fd9d4c3805a889a1
phamkimthoa <kimthoa16042004@gmail.com>
2024-05-25 07:55:03 +0700

    fix : import in Root.txt

--- src/page/Root.tsx
+++ src/page/Root.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { AppShell } from "@mantine/core";
 import Navbar from "../component/navbar/Navbar";
-import { Footer } from "../component/layout/footer/Footer";
+import { Footer } from "../component/layout/Footer/Footer";
 
 export default function Root() {
   return (
