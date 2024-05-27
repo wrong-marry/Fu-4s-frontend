06ea6ca281563d2422883cbb6dab03f5e5117508
Quynh4 <nofomtre@gmail.com>
2024-05-27 12:15:22 +0700

    update: post detail

--- src/page/post/PostPage.tsx
+++ src/page/post/PostPage.tsx
@@ -1,114 +1,60 @@
-import { Post } from "../search/SearchPage";
-import { Card, Image, Text, Group, RingProgress, Badge } from "@mantine/core";
-import classes from "./CardWithStats.module.css";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios, { AxiosResponse } from "axios";
+import {
+  Image,
+  Text,
+  Group,
+  RingProgress,
+  Badge,
+  Box,
+  Card,
+  CardSection,
+  Divider,
+  Flex,
+  Center,
+  Container,
+  Title,
+} from "@mantine/core";
 
-interface PostData {
-  id: string;
+import { format } from "date-fns";
+import { useParams } from "react-router-dom";
+import LearningMaterialDetail from "../../component/learning-material/LearningMaterialDetail";
+
+interface Post {
+  id: number;
+  postTime: string;
   title: string;
+  status: string | null;
   username: string;
-  postTime: Date;
   subjectCode: string;
-  isTest: boolean;
+  test: boolean;
 }
 
-export function CardWithStats() {
+const PostPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
-  const [data, setData] = useState<PostData | null>(null);
+
+  const [post, setPost] = useState<Post | null>(null);
 
   useEffect(() => {
-    async function fetchPost(id: string) {
-      const response = await axios.get<PostData>(
-        `http://localhost:8080/api/v1/post/get?id=${id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        }
-      );
-      setData(response.data);
-    }
-    if (!data) {
-      fetchPost(id!);
-    }
-  }, [id, data]);
+    const fetchPost = async () => {
+      try {
+        const response: AxiosResponse<Post> = await axios.get(
+          `http://localhost:8080/api/v1/post/get?id=${id}`
+        );
+        setPost(response.data);
+      } catch (error) {
+        console.error("Error fetching post:", error);
+      }
+    };
 
-  const footerItems = data ? (
-    <>
-      <div key="Post">
-        <Text size="xs" color="dimmed">
-          Uploader
-        </Text>
-        <Text fw={500} size="sm">
-          {data.username}
-        </Text>
-      </div>
-      <div key="Time">
-        <Text size="xs" color="dimmed">
-          Post time
-        </Text>
-        <Text fw={500} size="sm">
-          {data.postTime.toISOString().substring(0, 10)}
-        </Text>
-      </div>
-      <div key="Subject">
-        <Text size="xs" color="dimmed">
-          Subject
-        </Text>
-        <Text fw={500} size="sm">
-          {data.subjectCode}
-        </Text>
-      </div>
-    </>
-  ) : (
-    <div>No data</div>
-  );
+    fetchPost();
+  }, [id]);
 
-  return (
-    <Card withBorder padding="lg" className={classes.card}>
-      <Card.Section>
-        <Image
-          src="https://cdn.shortpixel.ai/spai/w_600+q_lossy+ret_img+to_webp/thesocialmediamonthly.com/wp-content/uploads/2018/01/quiz.jpg"
-          alt="QuizPlash"
-          height={30}
-        />
-      </Card.Section>
+  if (!post) {
+    return <div>Loading...</div>;
+  }
 
-      <Group justify="space-between" mt="xl">
-        <Text fz="sm" fw={700} className={classes.title}>
-          {data?.title}
-        </Text>
-        <Group gap={5}>
-          <Text fz="xs" c="dimmed">
-            80% completed
-          </Text>
-          <RingProgress
-            size={18}
-            thickness={2}
-            sections={[{ value: 80, color: "blue" }]}
-          />
-        </Group>
-        {data?.isTest ? (
-          <Badge color="indigo">Mock Test</Badge>
-        ) : (
-          <Badge color="pink">Learning material</Badge>
-        )}
-      </Group>
-      <Text mt="sm" mb="md" c="dimmed" fz="xs">
-        56 km this month • 17% improvement compared to last month • 443 place in
-        global scoreboard
-      </Text>
-      <Card.Section className={classes.footer}>{footerItems}</Card.Section>
-    </Card>
-  );
-}
+  return <>{!post.test && <LearningMaterialDetail />}</>;
+};
 
-export default function PostPage() {
-  return (
-    <Card>
-      <CardWithStats />
-    </Card>
-  );
-}
+export default PostPage;
