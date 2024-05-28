150880bd8cb6e9c8105fb9ce5de35001b233e40d
Quynh4 <nofomtre@gmail.com>
2024-05-28 20:13:26 +0700

    add: moment

---
+++ src/component/notification/NotificationCard.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Moment from "react-moment";
+import "moment/locale/vi";
+import {
+  Badge,
+  Box,
+  Grid,
+  IconButton,
+  Menu,
+  Stack,
+  Typography,
+} from "@mui/material";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+const PAGE_SIZE = 5;
+const NotificationCard: React.FC = () => {
+  return (
+    <>
+      <div>hello</div>
+    </>
+  );
+};
+
+export default NotificationCard;
--- src/page/Root.tsx
+++ src/page/Root.tsx
@@ -1,20 +1,20 @@
 import { Outlet, useNavigation } from "react-router-dom";
 import { AppShell } from "@mantine/core";
 import Navbar from "../component/navbar/Navbar";
-import {useEffect} from "react";
-import {nprogress} from "@mantine/nprogress";
-import { Footer } from "../component/layout/footer/Footer";
+import { useEffect } from "react";
+import { nprogress } from "@mantine/nprogress";
+import { Footer } from "../component/layout/Footer/Footer";
 
 export default function Root() {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    useEffect(() => {
-        if (navigation.state === "loading") {
-            nprogress.start();
-        } else {
-            nprogress.complete();
-        }
-    }, [navigation.state]);
+  useEffect(() => {
+    if (navigation.state === "loading") {
+      nprogress.start();
+    } else {
+      nprogress.complete();
+    }
+  }, [navigation.state]);
   return (
     <AppShell>
       <Navbar />
