4d3f3e848704569e66311320add7c2f9a75859fc
phamkimthoa <kimthoa16042004@gmail.com>
2024-06-03 00:08:39 +0700

    add : NavbarNested in Admin Page

--- src/page/manageUser-page/ManageUserPage.tsx
+++ src/page/manageUser-page/ManageUserPage.tsx
@@ -1,11 +1,44 @@
+import { useState } from "react";
+import { Grid, Drawer, Burger } from "@mantine/core";
+import {NavbarNested } from "../../component/manageUser/navbar-nested/navbar-nested";
 import DashboardSection from "../../component/manageUser/show-customer";
 import TableUser from "../../component/manageUser/user-list";
+import { useMediaQuery } from "@mantine/hooks";
 
 export default function ManageUser() {
+	// Sử dụng useMediaQuery để xác định kích thước màn hình
+	const isSmallScreen = useMediaQuery("(max-width: 1100px)");
+	const isMediumScreen = useMediaQuery("(max-width: 1024px)");
+	const [drawerOpened, setDrawerOpened] = useState(false);
+
+	const handleBurgerClick = () => {
+		setDrawerOpened((prev) => !prev);
+	};
+
 	return (
 		<>
-		<DashboardSection/>
-		<TableUser/>
+			<Grid>
+				<Grid.Col span={isSmallScreen ? 12 : isMediumScreen ? 3 : 2.6}>
+					{isSmallScreen ? (
+						<Burger opened={drawerOpened} onClick={handleBurgerClick} />
+					) : (
+						<NavbarNested />
+					)}
+				</Grid.Col>
+				<Grid.Col span={isSmallScreen ? 12 : isMediumScreen ? 9 : 9}>
+					<DashboardSection />
+					<TableUser />
+				</Grid.Col>
+			</Grid>
+
+			<Drawer
+				opened={drawerOpened}
+				onClose={() => setDrawerOpened(false)}
+				padding="md"
+				size="100%"
+			>
+				<NavbarNested />
+			</Drawer>
 		</>
 	);
 }
