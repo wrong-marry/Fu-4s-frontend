82c3060a48a575c620c0752e7b5df7beabab461e
Hieutritue <124784640+Hieutritue@users.noreply.github.com>
2024-07-16 18:33:30 +0700

    quen roi

---
+++ src/page/user-post/EditMockTestPage.tsx
@@ -0,0 +1,21 @@
+import {EditMockTestForm} from "../../component/user-post/EditMockTestForm.tsx";
+import {useNavigate, useParams} from "react-router-dom";
+import {isValidUser} from "../../util/ValidUser.tsx";
+
+export default function EditMockTestPage() {
+    const {id} = useParams<{ id: string }>();
+    const navigate = useNavigate();
+
+    isValidUser(localStorage.getItem("username"), id)
+        .then((res) => {
+            if (res.status != 200) navigate("/forbidden");
+            return res.json();
+        })
+        .then((data) => {
+            if (data == false) navigate("/forbidden");
+        })
+
+    return <>
+        <EditMockTestForm/>
+    </>
+}
