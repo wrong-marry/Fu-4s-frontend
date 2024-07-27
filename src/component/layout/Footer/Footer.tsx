63741c348856ac15ac7bd68b5e6d3c183a7ecdca
Quynh4 <nofomtre@gmail.com>
2024-07-27 16:45:18 +0700

    update: landing page

--- src/component/layout/Footer/Footer.tsx
+++ src/component/layout/Footer/Footer.tsx
@@ -4,33 +4,32 @@ import { MantineLogo } from '@mantinex/mantine-logo';
 import classes from './Footer.module.css';
 
 const data = [
-  {
-    title: 'About',
-    links: [
-      { label: 'Features', link: '#' },
-      { label: 'Pricing', link: '#' },
-      { label: 'Support', link: '#' },
-      { label: 'Forums', link: '#' },
-    ],
-  },
-  {
-    title: 'Project',
-    links: [
-      { label: 'Contribute', link: '#' },
-      { label: 'Media assets', link: '#' },
-      { label: 'Changelog', link: '#' },
-      { label: 'Releases', link: '#' },
-    ],
-  },
-  {
-    title: 'Community',
-    links: [
-      { label: 'Join Discord', link: '#' },
-      { label: 'Follow on Twitter', link: '#' },
-      { label: 'Email newsletter', link: '#' },
-      { label: 'GitHub discussions', link: '#' },
-    ],
-  },
+	{
+		title: "About",
+		links: [
+			{ label: "Features", link: "#" },
+			{ label: "Pricing", link: "#" },
+			{ label: "Support", link: "#" },
+		],
+	},
+	{
+		title: "Project",
+		links: [
+			{ label: "Contribute", link: "#" },
+			{ label: "Media assets", link: "#" },
+			{ label: "Changelog", link: "#" },
+			{ label: "Releases", link: "#" },
+		],
+	},
+	{
+		title: "Community",
+		links: [
+			{ label: "Join Discord", link: "#" },
+			{ label: "Follow on Twitter", link: "#" },
+			{ label: "Email newsletter", link: "#" },
+			{ label: "GitHub discussions", link: "#" },
+		],
+	},
 ];
 
 export function Footer() {
